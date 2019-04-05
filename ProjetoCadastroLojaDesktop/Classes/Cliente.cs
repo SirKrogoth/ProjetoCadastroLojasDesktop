265c392d01a6042162faae5b21b24da7a399287f
João Rafael Fagundes Menezes <joaorafael652@outlook.com>
2019-04-05 05:35:32 -0300

    Criação de exceção Personalizada.

--- ProjetoCadastroLojaDesktop/Classes/Cliente.cs
+++ ProjetoCadastroLojaDesktop/Classes/Cliente.cs
@@ -8,8 +8,36 @@ namespace ProjetoCadastroLojaDesktop
 {
     public partial class Cliente
     {
-        public int? codigo { get; set; }
-        public string nome { get; set; }
+        //public int? codigo { get; set; }
+        private int _codigo;
+        public int codigo
+        {
+            get
+            {
+                return _codigo;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ProjetoCadastroLojaDesktop.Excecoes.ValidacaoException("O código informado para este cliente está inválido. Favor, revise as informações lançadas.");
+                    _codigo = 0;
+                }
+            }
+        }
+        private string _nome;
+        public string nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if(value.Length <= 3)
+                    throw new ProjetoCadastroLojaDesktop.Excecoes.ValidacaoException("Não é possível inserir um nome com menos de 4 caracteres.");
+            }
+        }
         public int? tipo { get; set; }
         public DateTime? dataCadastro { get; set; }
 
--- ProjetoCadastroLojaDesktop/Program.cs
+++ ProjetoCadastroLojaDesktop/Program.cs
@@ -10,45 +10,52 @@ namespace ProjetoCadastroLojaDesktop
     {
         static void Main(string[] args)
         {
-            Cliente cliente = new Cliente();
-
-            cliente.codigo = 1;
-
-            cliente.nome = "joAO RAFAEL".PrimeiraMaiuscula();
-            cliente.tipo = 1;
-            cliente.dataCadastro = new DateTime(1989, 03, 14);
-
-            using (Cliente cliente2 = new Cliente())
+            try
             {
-                cliente2.nome = "CEBOLA".PrimeiraMaiuscula();
-                //Ao finalizar, irá chamar automaticamente o método DISPOSE
-            }            
+                Cliente cliente = new Cliente();
 
-            Classes.Contato contato1 = new Classes.Contato();
+                cliente.codigo = 1;
 
-            contato1.codigo = 1;
-            contato1.dadosContato = "589-999999";
-            contato1.tipo = "telefone";
+                cliente.nome = "EL".PrimeiraMaiuscula();
+                cliente.tipo = 1;
+                cliente.dataCadastro = new DateTime(1989, 03, 14);
 
-            Classes.Contato contato2 = new Classes.Contato();
+                using (Cliente cliente2 = new Cliente())
+                {
+                    cliente2.nome = "CEBOLA".PrimeiraMaiuscula();
+                    //Ao finalizar, irá chamar automaticamente o método DISPOSE
+                }
 
-            contato2.codigo = 2;
+                Classes.Contato contato1 = new Classes.Contato();
 
-            //Recebe o contato2.codigo e irá enviar o valor para o método Metade
-            int codigoMetadeCliente = contato2.codigo.Metade();
+                contato1.Codigo = 1;
+                contato1.dadosContato = "589-999999";
+                contato1.tipo = "telefone";
 
-            contato2.dadosContato = "joao@teleconsistemas.com.br";
-            contato2.tipo = "email";
+                Classes.Contato contato2 = new Classes.Contato();
 
-            cliente.contatos = new List<Classes.Contato>();
-            cliente.contatos.Add(contato1);
-            cliente.contatos.Add(contato2);
+                contato2.Codigo = 2;
 
-            Classes.Contato contatoEncontrado = cliente.contatos.FirstOrDefault(x => x.tipo == "telefone");
-            Console.WriteLine(contatoEncontrado.dadosContato);
-            Console.WriteLine("Cliente com primeira Maiuscula: " + cliente.nome);
-            
+                //Recebe o contato2.codigo e irá enviar o valor para o método Metade
+                int codigoMetadeCliente = contato2.Codigo.Metade();
 
+                contato2.dadosContato = "joao@teleconsistemas.com.br";
+                contato2.tipo = "email";
+
+                cliente.contatos = new List<Classes.Contato>();
+                cliente.contatos.Add(contato1);
+                cliente.contatos.Add(contato2);
+
+                Classes.Contato contatoEncontrado = cliente.contatos.FirstOrDefault(x => x.tipo == "telefone");
+                Console.WriteLine(contatoEncontrado.dadosContato);
+                Console.WriteLine("Cliente com primeira Maiuscula: " + cliente.nome);
+            }
+            catch (ProjetoCadastroLojaDesktop.Excecoes.ValidacaoException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }                        
+           
             Console.ReadKey();
         }
     }
